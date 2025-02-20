9d720d0b389310aacace38cea3f284d69de62c73
eogus623 <eogus623@gmail.com>
2025-02-20 16:32:03 +0900

    version 2

--- src/AddDemo.java
+++ src/AddDemo.java
@@ -1,6 +1,6 @@
 public class AddDemo {
   public static void main(String[] args) {
-    int j = 0, i = 1;
+    int j = 0, i = 2;
     System.out.printf("연산전 : j = %d, i = %d \n", j, i);
     j = i++;
     System.out.printf("연산후 : j = %d, i = %d", j, i);
